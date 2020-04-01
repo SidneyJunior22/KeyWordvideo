7ea35a26217f5726f31b2c91b96d36763156d391
Sidney Júnior <smjunior_22@hotmail.com>
2020-04-01 02:34:06 +0100

    Incomplete

--- robots/text.js
+++ robots/text.js
@@ -2,12 +2,23 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apiKey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+ 
+var nlu = new NaturalLanguageUnderstandingV1({
+  apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
+
+
 async function robot(content) {
   await fetchContentFromWikipedia(content)
   sanitizedContent(content)
   breakContentIntoSentences(content)
-
-  
+  limitMaximumSentences(content)
+  await fetchKeywordlOfAllSentences(content)
 
   async function fetchContentFromWikipedia(content) {
     const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -17,6 +28,7 @@ async function robot(content) {
 
     content.sourceContentOrigin = wikipediaContent.content
   }
+
   function sanitizedContent(content){
     const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOrigin)
     const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
@@ -49,8 +61,36 @@ async function robot(content) {
         images: []
       })
     })
- 
+  }
 
+  function limitMaximumSentences(content) {
+    content.sentences = content.sentences.slice(0,content.maximumSentences)
+  }
+
+  async function fetchKeywordlOfAllSentences(content) {
+    for (const sentence of content.sentences) {
+      sentence.keywords = await fetchWatsonAndReturnKeuwords(sentence.text)
+    }
+  }
+
+  async function fetchWatsonAndReturnKeuwords(sentence) {
+    return new promise((resolve, reject) => { 
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {}
+        }
+      }, (error, response) => {
+        if (error) {
+          throw error
+        }
+        const keywords = response.keywords.map((keyword) => {
+          return keyword.text
+        })
+        resolve(keywords)
+      })
+    })
   }
 }
+
 module.exports = robot
